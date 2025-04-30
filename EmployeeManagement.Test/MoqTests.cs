d4e4247336908a2b752212b2028fa5e56e4dc13a
Njaaga-Gakure <njaagagakure@gmail.com>
2025-04-30 12:52:42 +0300

    Configure mock object return value

--- EmployeeManagement.Test/MoqTests.cs
+++ EmployeeManagement.Test/MoqTests.cs
@@ -22,12 +22,16 @@ namespace EmployeeManagement.Test
         public void CreateInternalEmployee_InternalEmployeeCreated_SuggestedBonusMustBeCalculated() { 
             var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
             var employeeFactoryMock = new Mock<EmployeeFactory>();
-            
+            //var employeeFactory = new EmployeeFactory(); 
+            // only overridable methods can be mocked
+            employeeFactoryMock.Setup(employeeFactory => employeeFactory.CreateEmployee("Brian", It.IsAny<string>(), null, false)).Returns(new InternalEmployee("Brian", "Njaaga", 5, 2500, false, 1)); 
+            employeeFactoryMock.Setup(employeeFactory => employeeFactory.CreateEmployee("Sandy", It.IsAny<string>(), null, false)).Returns(new InternalEmployee("Sandy", "Wanjiru", 5, 2500, false, 1)); 
+            employeeFactoryMock.Setup(employeeFactory => employeeFactory.CreateEmployee("Jeff", It.Is<string>(value => value.Contains('a')), null, false)).Returns(new InternalEmployee("Jeff", "Waaaah", 5, 2500, false, 1)); 
             var employeeService = new EmployeeService(employeeManagementTestDataRepository, employeeFactoryMock.Object);
 
-            decimal suggestedBonus = 200;
+            decimal suggestedBonus = 1000;
 
-            var employee = employeeService.CreateInternalEmployee("Brian", "Gakure"); 
+            var employee = employeeService.CreateInternalEmployee("Jeff", "Gkure"); 
 
             Assert.Equal(suggestedBonus, employee.SuggestedBonus); 
         }
