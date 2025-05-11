0682e382df0143996ae6b661a260cdd3baaeebb7
Njaaga-Gakure <njaagagakure@gmail.com>
2025-05-11 14:15:43 +0300

    Test With HttpContext.User

--- EmployeeManagement.Test/DemoInternalEmployeeControllerTests.cs
+++ EmployeeManagement.Test/DemoInternalEmployeeControllerTests.cs
@@ -3,24 +3,26 @@ using AutoMapper;
 using EmployeeManagement.Business;
 using EmployeeManagement.Controllers;
 using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System.Security.Claims;
 
 namespace EmployeeManagement.Test
 {
-    // not to self: Add this test only when the controller does not have the [ApiController] attribute
     public class DemoInternalEmployeeControllerTests
     {
+     
+    // not to self: Add this test only when the controller does not have the [ApiController] attribute
         [Fact]
         public async Task CrreateInternalEmployee_InvalidInput_MustReturnBadRequest()
         {
 
-            // arrange
+          
+            var internalEmployeeForCreationDtoMock = new Mock<InternalEmployeeForCreationDto>();
             var employeeServiceMock = new Mock<IEmployeeService>();
             var mapperMock = new Mock<IMapper>();
             var demoInternalEmployeeController = new DemoInternalEmployeeController(employeeServiceMock.Object, mapperMock.Object);
-
-            var internalEmployeeForCreationDtoMock = new Mock<InternalEmployeeForCreationDto>();
             demoInternalEmployeeController.ModelState.AddModelError("FirstName", "Required");
 
             // act
@@ -28,8 +30,54 @@ namespace EmployeeManagement.Test
 
             // assert 
             var actionResult = Assert.IsType<ActionResult<InternalEmployeeDto>>(result); 
-            var badRequestResult = Assert.IsType<BadRequestObjectResult>(actionResult.Result);
-        
+            Assert.IsType<BadRequestObjectResult>(actionResult.Result);
+      
+        }
+
+        [Fact]
+        public void GetProtectedInternalEmployee_GetActionForUserInAndminRole_MustRedirectToGetInternalEmployeesOnProtectedInternalEmployees() {
+            var employeeServiceMock = new Mock<IEmployeeService>();
+            var mapperMock = new Mock<IMapper>();
+            var demoInternalEmployeeController = new DemoInternalEmployeeController(employeeServiceMock.Object, mapperMock.Object);
+            var userClaims = new List<Claim>() {
+                new(ClaimTypes.Name, "Karen"),
+                new(ClaimTypes.Role, "Admin"),
+            };
+            var claimsIdentity = new ClaimsIdentity(userClaims, "unitTest");
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            var httpContext = new DefaultHttpContext() { 
+                User = claimsPrincipal
+            };
+            demoInternalEmployeeController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+            var result = demoInternalEmployeeController.GetProtectedInternalEmployee();
+            var actionResult = Assert.IsType<IActionResult>(result, exactMatch: false);
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("GetInternalEmployees", redirectToActionResult.ActionName);
+            Assert.Equal("ProtectedInternalEmployee", redirectToActionResult.ControllerName); 
+
+
+        }
+        [Fact]
+        public void GetProtectedInternalEmployee_GetActionForUserInAndminRole_MustRedirectToGetInternalEmployeesOnProtectedInternalEmployees_WithMoq() {
+            var employeeServiceMock = new Mock<IEmployeeService>();
+            var mapperMock = new Mock<IMapper>();
+            var demoInternalEmployeeController = new DemoInternalEmployeeController(employeeServiceMock.Object, mapperMock.Object);
+            var mockPrincipal = new Mock<ClaimsPrincipal>();
+            mockPrincipal.Setup(principal => principal.IsInRole(It.Is<string>(role => role == "Admin"))).Returns(true);
+            var httpContextMock = new Mock<HttpContext>(); 
+            httpContextMock.Setup(httpContext => httpContext.User).Returns(mockPrincipal.Object);
+            demoInternalEmployeeController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = httpContextMock.Object
+            };
+            var result = demoInternalEmployeeController.GetProtectedInternalEmployee();
+            var actionResult = Assert.IsType<IActionResult>(result, exactMatch: false);
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("GetInternalEmployees", redirectToActionResult.ActionName);
+            Assert.Equal("ProtectedInternalEmployee", redirectToActionResult.ControllerName); 
         }
     }
 }
--- EmployeeManagement/Controllers/DemoInternalEmployeeController.cs
+++ EmployeeManagement/Controllers/DemoInternalEmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using EmployeeManagement.Business;
 using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
@@ -9,28 +10,39 @@ namespace EmployeeManagement.Controllers
     [Route("api/demointernalemployees")]
     public class DemoInternalEmployeeController(IEmployeeService employeeService, IMapper mapper) : ControllerBase
     {
-       
-            private readonly IEmployeeService _employeeService = employeeService;
-            private readonly IMapper _mapper = mapper;
 
-            [HttpPost]
-            public async Task<ActionResult<InternalEmployeeDto>> CreateInternalEmployee(InternalEmployeeForCreationDto internalEmployeeForCreation)
+        private readonly IEmployeeService _employeeService = employeeService;
+        private readonly IMapper _mapper = mapper;
+
+        [HttpPost]
+        public async Task<ActionResult<InternalEmployeeDto>> CreateInternalEmployee(InternalEmployeeForCreationDto internalEmployeeForCreation)
+        {
+            if (!ModelState.IsValid)
             {
-                if(!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
-                // create an internal employee entity with default values filled out
-                // and the values inputted via the POST request
-                var internalEmployee =
-                        await _employeeService.CreateInternalEmployeeAsync(
-                            internalEmployeeForCreation.FirstName, internalEmployeeForCreation.LastName);
-
-                // persist it
-                await _employeeService.AddInternalEmployeeAsync(internalEmployee);
-
-                // return created employee after mapping to a DTO
-                return CreatedAtAction("GetInternalEmployee", _mapper.Map<InternalEmployeeDto>(internalEmployee), new { employeeId = internalEmployee.Id });
+                return BadRequest(ModelState);
             }
+            // create an internal employee entity with default values filled out
+            // and the values inputted via the POST request
+            var internalEmployee =
+                    await _employeeService.CreateInternalEmployeeAsync(
+                        internalEmployeeForCreation.FirstName, internalEmployeeForCreation.LastName);
+
+            // persist it
+            await _employeeService.AddInternalEmployeeAsync(internalEmployee);
+
+            // return created employee after mapping to a DTO
+            return CreatedAtAction("GetInternalEmployee", _mapper.Map<InternalEmployeeDto>(internalEmployee), new { employeeId = internalEmployee.Id });
         }
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetProtectedInternalEmployee()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                return RedirectToAction("GetInternalEmployees", "ProtectedInternalEmployee");
+            }
+            return RedirectToAction("GetInternalEmployees", "InternalEmployees");
+        }
+
+    }
 }
