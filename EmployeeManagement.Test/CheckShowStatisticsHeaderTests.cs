188c44fba3da3581d674dd72055415691bb44ece
Njaaga-Gakure <njaagagakure@gmail.com>
2025-05-11 15:58:27 +0300

    Test Asp.net core filters

---
+++ EmployeeManagement.Test/CheckShowStatisticsHeaderTests.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManagement.ActionFilters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EmployeeManagement.Test
+{
+    public class CheckShowStatisticsHeaderTests
+    {
+
+        [Fact]
+        public void OnActionExecuting_InvokeWithoutShoeStatisticsHeader_ReturnsBadRequest()
+        {
+            var checkShowStatisticsHeaderActionFilter = new CheckShowStatisticsHeader(); 
+            var httpContext = new DefaultHttpContext();
+            var actionContext = new ActionContext(httpContext, new(), new(), new());
+            var actionExecutingContext = new ActionExecutingContext(actionContext, [], new Dictionary<string, object?>(), controller: null); 
+            checkShowStatisticsHeaderActionFilter.OnActionExecuting(actionExecutingContext);
+            Assert.IsType<BadRequestResult>(actionExecutingContext.Result); 
+        }
+    }
+}
