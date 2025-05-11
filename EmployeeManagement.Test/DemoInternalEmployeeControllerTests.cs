4f2b72d69b00547a0f713a1e752658a9b3bd3630
Njaaga-Gakure <njaagagakure@gmail.com>
2025-05-11 12:23:15 +0300

    Test validation and ModelState

---
+++ EmployeeManagement.Test/DemoInternalEmployeeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿
+using AutoMapper;
+using EmployeeManagement.Business;
+using EmployeeManagement.Controllers;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace EmployeeManagement.Test
+{
+    // not to self: Add this test only when the controller does not have the [ApiController] attribute
+    public class DemoInternalEmployeeControllerTests
+    {
+        [Fact]
+        public async Task CrreateInternalEmployee_InvalidInput_MustReturnBadRequest()
+        {
+
+            // arrange
+            var employeeServiceMock = new Mock<IEmployeeService>();
+            var mapperMock = new Mock<IMapper>();
+            var demoInternalEmployeeController = new DemoInternalEmployeeController(employeeServiceMock.Object, mapperMock.Object);
+
+            var internalEmployeeForCreationDtoMock = new Mock<InternalEmployeeForCreationDto>();
+            demoInternalEmployeeController.ModelState.AddModelError("FirstName", "Required");
+
+            // act
+            var result = await demoInternalEmployeeController.CreateInternalEmployee(internalEmployeeForCreationDtoMock.Object);
+
+            // assert 
+            var actionResult = Assert.IsType<ActionResult<InternalEmployeeDto>>(result); 
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(actionResult.Result);
+        
+        }
+    }
+}
---
+++ EmployeeManagement/Controllers/DemoInternalEmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using EmployeeManagement.Business;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    // no api controller atrribute
+    [Route("api/demointernalemployees")]
+    public class DemoInternalEmployeeController(IEmployeeService employeeService, IMapper mapper) : ControllerBase
+    {
+       
+            private readonly IEmployeeService _employeeService = employeeService;
+            private readonly IMapper _mapper = mapper;
+
+            [HttpPost]
+            public async Task<ActionResult<InternalEmployeeDto>> CreateInternalEmployee(InternalEmployeeForCreationDto internalEmployeeForCreation)
+            {
+                if(!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                // create an internal employee entity with default values filled out
+                // and the values inputted via the POST request
+                var internalEmployee =
+                        await _employeeService.CreateInternalEmployeeAsync(
+                            internalEmployeeForCreation.FirstName, internalEmployeeForCreation.LastName);
+
+                // persist it
+                await _employeeService.AddInternalEmployeeAsync(internalEmployee);
+
+                // return created employee after mapping to a DTO
+                return CreatedAtAction("GetInternalEmployee", _mapper.Map<InternalEmployeeDto>(internalEmployee), new { employeeId = internalEmployee.Id });
+            }
+        }
+}
